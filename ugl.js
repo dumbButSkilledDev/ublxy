79aa8914599fbcba712171c64e1c7dbbd059aa48
dumbButSkilledDev <111146406+dumbButSkilledDev@users.noreply.github.com>
2025-09-03 18:34:38 -0700

    Add initial implementation of UGL game launcher

---
+++ ugl.js
@@ -0,0 +1,34 @@
+
+// ublxy thingy
+
+var UBCDN = "https://cdn.jsdelivr.net/gh/dumbButSkilledDev/ublxy/";
+
+async function fetchWrap(url) {
+    var req = await fetch(url);
+    var resp = await req.json();
+    return resp;
+}
+
+var uglUnityConf = {Module:{onRuntimeInitialized: function() { console.log("[UGL] were loaded bruh!") }}};
+
+var uglGameInstance;
+
+function launchGameUnity(jsonUrl) {
+     console.log("[UGL] actually launching game...");
+     uglGameInstance = UnityLoader.instantiate("gameContainer", jsonUrl, uglUnityConf);
+}
+
+async function launchGame(index) {
+    var gameList = await fetchWrap(UBCDN + "games.json");
+    console.log("[UGL] launching game of index: " + index);
+
+    var game = gameList[index];
+    console.log("[UGL] game name: " + game.name);
+    console.log("[UGL] load info: " + game.loadInfo);
+    console.log("[UGL] load data: " + game.loadData);
+    console.log("[UGL] game launch method: " + game.loadInfo);
+
+    if (game.loadInfo == "unity") {
+        launchGameUnity(game.loadData);
+    }
+}
